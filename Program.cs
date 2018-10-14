0492a9f38f6d628d6722441395ed6d55d7d2dad0
Sevitte <sevitte@o2.pl>
2018-10-14 18:01:46 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,7 @@ namespace Alg1
 {
     class Program
     {
-        public static double power(double x, int y)
+        public static double Power(double x, int y)
         {
             int counter;
             double x2 = x;
@@ -25,14 +25,14 @@ namespace Alg1
             return x;
         }
 
-        public static double factorial(double x)
+        public static double Factorial(double x)
         {
             if (x == 0)
                 return 1;
-            return x*factorial(x-1);
+            return x*Factorial(x-1);
         }
 
-        public static double abs(double x)
+        public static double Abs(double x)
         {
             if (x >= 0)
                 return x;
@@ -43,7 +43,7 @@ namespace Alg1
 
         //1
 
-        public static double taylorCos(double x)
+        public static double TaylorCos(double x)
         {
             double result = 0;
             double expression = 0;
@@ -51,16 +51,15 @@ namespace Alg1
             int n = 0;
             do
             {
-                expression = (power(-1, n) * power(x, 2 * n)) / factorial(2 * n);
+                expression = (Power(-1, n) * Power(x, 2 * n)) / Factorial(2 * n);
                 result = result + expression;
                 n++;
             }
-            while (abs(expression) > epsylon);
-            Console.WriteLine(n);
+            while (Abs(expression) > epsylon);
             return result;
         }
 
-        public static double taylorE(double x)
+        public static double TaylorE(double x)
         {
             double result = 0;
             double expression = 0;
@@ -68,18 +67,16 @@ namespace Alg1
             int n = 0;
             do
             {
-                expression = power(x, n) / factorial(n);
+                expression = Power(x, n) / Factorial(n);
                 result = result + expression;
                 n++;
             }
-            while (abs(expression) > epsylon);
-            Console.WriteLine("e");
-            Console.WriteLine(n);
+            while (Abs(expression) > epsylon);
             return result;
         }
 
         //2
-        public static double taylorCos2(double x)
+        public static double TaylorCos2(double x)
         {
             double result = 0;
             double expression = 0;
@@ -88,12 +85,11 @@ namespace Alg1
             List<double> expressions = new List<double>();
             do
             {
-                expression = (power(-1, n) * power(x, 2 * n)) / factorial(2 * n);
+                expression = (Power(-1, n) * Power(x, 2 * n)) / Factorial(2 * n);
                 expressions.Add(expression);
                 n++;
             }
-            while (abs(expression) > epsylon);
-            Console.WriteLine(n);
+            while (Abs(expression) > epsylon);
             for (int i = n-1; i >= 0; i--)
             {
                 result = result + expressions[i];
@@ -101,7 +97,7 @@ namespace Alg1
             return result;
         }
 
-        public static double taylorE2(double x)
+        public static double TaylorE2(double x)
         {
             double result = 0;
             double expression = 0;
@@ -110,11 +106,11 @@ namespace Alg1
             List<double> expressions = new List<double>();
             do
             {
-                expression = power(x, n) / factorial(n);
+                expression = Power(x, n) / Factorial(n);
                 expressions.Add(expression);
                 n++;
             }
-            while (abs(expression) > epsylon);
+            while (Abs(expression) > epsylon);
             Console.WriteLine("e" + n);
             for (int i = n-1; i >= 0; i--)
             {
@@ -124,7 +120,7 @@ namespace Alg1
         }
 
         //3
-        public static double taylorCos3(double x)
+        public static double TaylorCos3(double x)
         {
             double result = 0;
             double epsylon = 0.000001;
@@ -134,16 +130,18 @@ namespace Alg1
             {
                 if (n == 0)
                     expression = 1;
-                else 
-                    expression = expression*(-x/(2*n));
-                n++;
+                if (n == 1)
+                    expression = -(Power(x, 2) / 2);
+                if (n > 1)
+                    expression = expression * (-(Power(x, 2)) / (n * (n - 1)));
+                n=n+2;
                 result = result + expression;
             }
-            while (abs(result* (-x / (2 * n))) > epsylon);
+            while (Abs(expression) > epsylon);
             return result;
         }
 
-        public static double taylorE3(double x)
+        public static double TaylorE3(double x)
         {
             double result = 0;
             double epsylon = 0.000001;
@@ -158,28 +156,78 @@ namespace Alg1
                 n++;
                 result = result + expression;
             }
-            while (abs(result * (x / n)) > epsylon);
+            while (Abs(expression) > epsylon);
             return result;
         }
 
+        //4
+        public static double TaylorCos4(double x)
+        {
+            double result = 0;
+            double epsylon = 0.000001;
+            double expression = 0;
+            List<double> expressions = new List<double>();
+            int n = 0;
+            do
+            {
+                if (n == 0)
+                    expression = 1;
+                if (n == 1)
+                    expression = -(Power(x, 2) / 2);
+                if (n > 1)
+                    expression = expression * (-(Power(x, 2)) / (n * (n - 1)));
+                n = n + 2;
+                expressions.Add(expression);
+            }
+            while (Abs(expression) > epsylon);
+            for ( int i = (n/2)-1 ; i >= 0; i--)
+            {
+                result = result + expressions[i];
+            }
+            return result;
+        }
+
+        public static double TaylorE4(double x)
+        {
+            double result = 0;
+            double epsylon = 0.000001;
+            double expression = 0;
+            List<double> expressions = new List<double>();
+            int n = 0;
+            do
+            {
+                if (n == 0)
+                    expression = 1;
+                else
+                    expression = expression * (x / n);
+                n++;
+                expressions.Add(expression);
+            }
+            while (Abs(expression) > epsylon);
+            for (int i = n - 1; i >= 0; i--)
+            {
+                result = result + expressions[i];
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
-            ////zapis do pliku z https://docs.microsoft.com/pl-pl/dotnet/standard/io/how-to-write-text-to-a-file
-            //string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            //double n = -4;
-            //using (StreamWriter outputFile = new StreamWriter(Path.Combine(mydocpath, "WriteLines3.txt")))
-            //{
-            //    do
-            //    {
-            //        outputFile.WriteLine(taylorE3(taylorCos3(n)) + ";" + Math.Pow(Math.E, Math.Cos(n)));
-            //        n = n + 0.000008;
-            //    }
-            //    while (n < 4);
-            //}
-
-
-            Console.WriteLine(taylorE3(taylorCos3(-1)));
-            Console.WriteLine(Math.Pow(Math.E, Math.Cos(-1)));
+            //zapis do pliku z https://docs.microsoft.com/pl-pl/dotnet/standard/io/how-to-write-text-to-a-file
+            string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            double n = -4;
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(mydocpath, "WriteLines.txt")))
+            {
+                do
+                {
+                    outputFile.WriteLine(TaylorE(TaylorCos(n)) + ";" + Math.Pow(Math.E, Math.Cos(n)));
+                    // n = n + 0.000008;
+                    // n = n + 0.08;
+                }
+                while (n < 4);
+            }
+            //test dla drukowania pojedynczych wyrazen
+            //Console.WriteLine(TaylorE3(TaylorCos3(-4)));
+            //Console.WriteLine(Math.Pow(Math.E, Math.Cos(-4)));
             Console.ReadLine();
         }
     }
